cbfb66317a941d343cf2813382fffc317ef661e2
Russ Cam <russ.cam@forloop.co.uk>
2023-06-21 23:30:00 +1000

    Add Documentation (#3)
    This commit adds documentation for the client.

---
+++ examples/Examples/Client.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using Qdrant.Grpc;
+
+namespace Examples;
+
+public class Client
+{
+	public void Create()
+	{
+		#region CreateClient
+		var address = GrpcChannel.ForAddress("http://localhost:6334");
+		var client = new QdrantGrpcClient(address);
+		#endregion
+	}
+}
